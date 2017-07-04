356af5bc1a6fae6f2a419e9cfe43ab8676ceee95
Mitchell Shelton <mitchell@Mitchells-MacBook-Pro.local>
2017-07-04 15:41:59 +1000

    unfinished server.js

--- chart/server.js
+++ chart/server.js
@@ -1,15 +1,15 @@
 var express = require('express');
 var app = express();
-//var React = require('react');
-//var ReactDOMServer = require('react-dom/server');
-//var Component = require('./src/App.js');
+var React = require('react');
+var ReactDOMServer = require('react-dom/server');
+var Component = require('./src/index.js');
 
 app.get('/', function(request, response) {
-  response.send('Hello world!\n');
-  response.send(html);
+  //response.send('Hello world!\n');
+  response.send(ReactDOMServer.renderToString(Component));
 });
 
-var PORT = 9000;
+var PORT = 3000;
 app.listen(PORT, function() {
   console.log('listening on ' + PORT);
 });
