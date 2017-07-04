2fba6c7139b11d54bd326649806d3b3227faf239
Mitchell Shelton <mitchell@Mitchells-MacBook-Pro.local>
2017-07-04 13:51:38 +1000

    changes to server.js

--- chart/server.js
+++ chart/server.js
@@ -1,21 +1,15 @@
-require('babel-register')({
-  presets: ['react']
-});
-
 var express = require('express');
 var app = express();
-var React = require('react');
-var ReactDOMServer = require('react-dom/server');
-var Component = require('./src/App.js');
+//var React = require('react');
+//var ReactDOMServer = require('react-dom/server');
+//var Component = require('./src/App.js');
 
 app.get('/', function(request, response) {
-  var html = ReactDOMServer.renderToString(
-    React.createElement(Component)
-  );
+  response.sned('Hello world!\n');
   response.send(html);
 });
 
-var PORT = 3000;
+var PORT = 9000;
 app.listen(PORT, function() {
-  console.log('http://localhost:' + PORT);
+  console.log('listening on ' + PORT);
 });
